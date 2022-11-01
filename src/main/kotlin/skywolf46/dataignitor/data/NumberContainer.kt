1bddd9b859255ffab7af1096c1648fd5633a2e19
skywolf46 <skywolf461@gmail.com>
2022-11-01 17:12:17 +0900

    feat: add number container to return unsigned data values

---
+++ src/main/kotlin/skywolf46/dataignitor/data/NumberContainer.kt
@@ -0,0 +1,25 @@
+package skywolf46.dataignitor.data
+
+data class NumberContainer<T : Number>(val data: T, val unsignedData: Any?) {
+    override fun toString(): String {
+        return unsignedData?.toString() ?: data.toString()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NumberContainer<*>
+
+        if (data != other.data) return false
+        if (unsignedData != other.unsignedData) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = data.hashCode()
+        result = 31 * result + (unsignedData?.hashCode() ?: 0)
+        return result
+    }
+}
