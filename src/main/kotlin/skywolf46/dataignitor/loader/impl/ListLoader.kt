06aa94ec05fcd6614e88b0efc134ceaf7cadb365
skywolf46 <skywolf461@gmail.com>
2022-11-02 00:14:57 +0900

    fix: fix wrong byte read of list size

--- src/main/kotlin/skywolf46/dataignitor/loader/impl/ListLoader.kt
+++ src/main/kotlin/skywolf46/dataignitor/loader/impl/ListLoader.kt
@@ -3,6 +3,7 @@ package skywolf46.dataignitor.loader.impl
 import skywolf46.dataignitor.data.SchemaErrorInfo
 import skywolf46.dataignitor.loader.SchemaDataLoader
 import skywolf46.dataignitor.util.YamlReader
+import skywolf46.dataignitor.util.readCInt32
 import java.io.DataInputStream
 
 object ListLoader : SchemaDataLoader<List<Any>> {
@@ -20,11 +21,10 @@ object ListLoader : SchemaDataLoader<List<Any>> {
         errors: SchemaErrorInfo
     ): List<Any> {
         val isFixedLength = schema.contains("length")
-//        val length = if (schema.contains("length")) schema.getInt("length").toLong() else (stream.readInt().toLong() and 0xffffffffL)
+        val length = if (schema.contains("length")) schema.getInt("length").toLong() else (stream.readCInt32())
         val itemSchema = schema.getSection("itemTypes")!!
         val itemSize = itemSchema.getInt("size")
         val lst = mutableListOf<Any>()
-        println("$itemSize bytes")
         for (x in 0L until itemSize) {
             lst += SchemaDataLoader.represent<Any>(stream, itemSchema, errors)
         }
