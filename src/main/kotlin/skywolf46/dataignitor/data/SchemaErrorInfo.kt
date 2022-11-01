b0e0ad21a06ea3b2845f5a8cf18140700143c056
skywolf46 <skywolf461@gmail.com>
2022-11-01 18:50:14 +0900

    feat: add SchemaErrorInfo

--- src/main/kotlin/skywolf46/dataignitor/data/SchemaErrorInfo.kt
+++ src/main/kotlin/skywolf46/dataignitor/data/SchemaErrorInfo.kt
@@ -2,7 +2,7 @@ package skywolf46.dataignitor.data
 
 import java.io.File
 
-class SchemaErrorInfo(val file: File, val schema: File) : ArrayList<SchemaErrorInfo.SchemaError>() {
+class SchemaErrorInfo(val file: File?, val schema: File?) : ArrayList<SchemaErrorInfo.SchemaError>() {
     data class SchemaError(val from: String, val message: String)
 
     fun addSchemaError(from: String, message: String) {
