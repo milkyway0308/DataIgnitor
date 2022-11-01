f8632a28d26ef11afd74b98f81e1e09df26a0140
skywolf46 <skywolf461@gmail.com>
2022-11-01 15:46:16 +0900

    feat: add SchemaErrorInfo to log validation errors

---
+++ src/main/kotlin/skywolf46/dataignitor/data/SchemaErrorInfo.kt
@@ -0,0 +1,11 @@
+package skywolf46.dataignitor.data
+
+import java.io.File
+
+class SchemaErrorInfo(val file: File, val schema: File) : ArrayList<SchemaErrorInfo.SchemaError>() {
+    data class SchemaError(val from: String, val message: String)
+
+    fun addSchemaError(from: String, message: String) {
+        add(SchemaError(from, message))
+    }
+}
