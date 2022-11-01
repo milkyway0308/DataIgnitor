284b73f2928f9768f6df79700018778dc9201e61
skywolf46 <skywolf461@gmail.com>
2022-11-01 15:36:23 +0900

    feat: add and fix companion method of SchemaDataLoader

--- src/main/kotlin/skywolf46/dataignitor/loader/SchemaDataLoader.kt
+++ src/main/kotlin/skywolf46/dataignitor/loader/SchemaDataLoader.kt
@@ -1,22 +1,28 @@
 package skywolf46.dataignitor.loader
 
+import skywolf46.dataignitor.exceptions.InvalidSchemaException
 import skywolf46.dataignitor.util.YamlReader
 import java.io.DataInputStream
 import java.io.InputStream
 
 interface SchemaDataLoader<T : Any> {
     companion object {
-        fun <T: Any> of(cls: Any) : SchemaDataLoader<T> {
+        fun <T : Any> of(cls: Class<out T>): SchemaDataLoader<T> {
             return TODO()
         }
 
-        fun <T: Any> of(key: String) : SchemaDataLoader<T> {
+        fun <T : Any> of(key: String): SchemaDataLoader<T> {
             return TODO()
         }
 
-        fun parse(section: YamlReader.YamlSection) : SchemaDataLoader<T> {
-            return TODO()
+        fun <T : Any> parse(deserializeTo: Class<T>, stream: DataInputStream, section: YamlReader.YamlSection): T {
+            return of(deserializeTo).readStream(stream, section)
+        }
+
+        fun <T : Any> parse(key: String, stream: DataInputStream, section: YamlReader.YamlSection): T {
+            return of<T>(key).readStream(stream, section)
         }
     }
+
     fun readStream(stream: DataInputStream, schema: YamlReader.YamlSection): T
 }
