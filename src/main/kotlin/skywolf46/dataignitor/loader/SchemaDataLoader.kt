d905b6a63b583822952f6a66a9994a4e4c6b4321
skywolf46 <skywolf461@gmail.com>
2022-11-01 17:13:04 +0900

    feat: add SchemaDataLoader#represent to parse full yaml

--- src/main/kotlin/skywolf46/dataignitor/loader/SchemaDataLoader.kt
+++ src/main/kotlin/skywolf46/dataignitor/loader/SchemaDataLoader.kt
@@ -31,6 +31,10 @@ interface SchemaDataLoader<T : Any> {
         ): T {
             return of<T>(key).readStream(stream, section, errors)
         }
+
+        fun <T : Any> represent(stream: DataInputStream, section: YamlReader.YamlSection, errors: SchemaErrorInfo): T {
+            return parse(section["type"]!!, stream, section, errors)
+        }
     }
 
     fun readStream(stream: DataInputStream, schema: YamlReader.YamlSection, errors: SchemaErrorInfo): T
