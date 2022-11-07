a4a49630ec6c63a4f1991fafad9fea6b7afc2573
skywolf46 <skywolf461@gmail.com>
2022-11-08 07:36:56 +0900

    fix: fix list loading failed when list is variable sized list

--- src/main/kotlin/skywolf46/dataignitor/loader/impl/ListLoader.kt
+++ src/main/kotlin/skywolf46/dataignitor/loader/impl/ListLoader.kt
@@ -8,17 +8,18 @@ import java.io.DataInputStream
 
 object ListLoader : SchemaDataLoader<YamlWrapper.YamlList> {
     override fun readStream(
-        stream: DataInputStream,
-        schema: YamlWrapper.YamlSection,
-        errors: SchemaErrorInfo
+        stream: DataInputStream, schema: YamlWrapper.YamlSection, errors: SchemaErrorInfo
     ): YamlWrapper.YamlList {
-        return loadFixedList(stream, schema, errors)
+        return if (schema.contains("fixedItemSize")) {
+            loadFixedList(stream, schema, errors)
+        } else {
+            loadVariableSizedList(stream, schema, errors)
+        }
     }
 
+
     private fun loadFixedList(
-        stream: DataInputStream,
-        schema: YamlWrapper.YamlSection,
-        errors: SchemaErrorInfo
+        stream: DataInputStream, schema: YamlWrapper.YamlSection, errors: SchemaErrorInfo
     ): YamlWrapper.YamlList {
         val isFixedLength = schema.contains("length")
         val length = if (schema.contains("length")) schema.getInt("length") else (stream.readCInt32())
@@ -30,4 +31,22 @@ object ListLoader : SchemaDataLoader<YamlWrapper.YamlList> {
         }
         return lst
     }
+
+    private fun loadVariableSizedList(
+        stream: DataInputStream, schema: YamlWrapper.YamlSection, errors: SchemaErrorInfo
+
+    ): YamlWrapper.YamlList {
+        val length = if (schema.contains("length")) schema.getInt("length") else (stream.readCInt32())
+        println("Length $length")
+        // Skip offsets.
+        @Suppress("UNUSED_VARIABLE") val listOffsets = Array(length) { stream.readCInt32() }
+        println(stream.available())
+        val lst = YamlWrapper.YamlList()
+        val itemSchema = schema.getSection("itemTypes")!!
+        for (x in 0 until length) {
+            lst += SchemaDataLoader.represent(stream, itemSchema, errors)
+        }
+        return lst
+    }
+
 }
