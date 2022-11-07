797dcc5c24cbac5856ce59b75df501f40dd8acfa
skywolf46 <skywolf461@gmail.com>
2022-11-08 06:00:19 +0900

    fix: fix wrong data reading logic of DictLoader

--- src/main/kotlin/skywolf46/dataignitor/loader/impl/DictLoader.kt
+++ src/main/kotlin/skywolf46/dataignitor/loader/impl/DictLoader.kt
@@ -1,8 +1,12 @@
 package skywolf46.dataignitor.loader.impl
 
 import skywolf46.dataignitor.data.SchemaErrorInfo
+import skywolf46.dataignitor.data.eve.KeyOffsetData
+import skywolf46.dataignitor.data.eve.KeyOffsetWithSizeData
 import skywolf46.dataignitor.loader.SchemaDataLoader
 import skywolf46.dataignitor.util.YamlWrapper
+import skywolf46.dataignitor.util.readCInt32
+import java.io.ByteArrayInputStream
 import java.io.DataInputStream
 
 object DictLoader : SchemaDataLoader<YamlWrapper.YamlSection> {
@@ -12,25 +16,90 @@ object DictLoader : SchemaDataLoader<YamlWrapper.YamlSection> {
         errors: SchemaErrorInfo
     ): YamlWrapper.YamlSection {
         val map = YamlWrapper.YamlSection()
-        val dataSize = stream.readInt()
-        val footerSize = stream.readInt()
-        val valueSchema = schema.getSection("valueTypes")!!
-        for (x in valueSchema.getKeys()) {
-            if (!valueSchema.isSection(x)) {
-
-                continue
-            }
-            map[x] = SchemaDataLoader.represent(stream, valueSchema.getSection(x)!!, errors)
+        val dataSize = stream.readCInt32() - 4
+        val cachedData = ByteArray(dataSize).apply(stream::read)
+        val footerSize = stream.readCInt32()
+        val dataStream = DataInputStream(ByteArrayInputStream(cachedData, 0, dataSize - footerSize))
+        val footerStream = DataInputStream(ByteArrayInputStream(cachedData, dataSize - footerSize, footerSize))
+        // isOptimized;
+        val isIntKeySchema = schema.getSection("keyTypes")?.get("type") == "int"
+        val dataAmount = footerStream.readCInt32()
+        if (isIntKeySchema) {
+            parseIntegerKeyMap(map, schema, dataStream, footerStream, errors, dataAmount)
+        } else {
+            parseDataKeyMap(map, schema, dataStream, footerStream, errors, dataAmount)
         }
-
-        // Data used for pyFSD implementation. Ignore it.
-//        val isFooterOptimized = schema.getSection("keyTypes")!!["type"] == "int"
-//        val footer =
-//            ListSchemaLoader.readStream(stream, schema.getSection("keyFooter")!!, errors) as List<Map<String, Int>>
-        // Ignore footer.
-        val data = ByteArray(dataSize).apply(stream::read)
-
         return map
     }
+
+    private fun parseIntegerKeyMap(
+        map: YamlWrapper.YamlSection,
+        configuration: YamlWrapper.YamlSection,
+        dataStream: DataInputStream,
+        footerStream: DataInputStream,
+        errors: SchemaErrorInfo,
+        amount: Int
+    ) {
+        if (configuration.getByContinuity("keyFooter.itemTypes.attributes.size") != null) {
+            parseIntegerKeyWithOffsetAndSize(map, configuration, dataStream, footerStream, errors, amount)
+        } else {
+            parseIntegerKeyWithOffset(map, configuration, dataStream, footerStream, errors, amount)
+        }
+    }
+
+    private fun parseIntegerKeyWithOffsetAndSize(
+        map: YamlWrapper.YamlSection,
+        configuration: YamlWrapper.YamlSection,
+        dataStream: DataInputStream,
+        footerStream: DataInputStream,
+        errors: SchemaErrorInfo,
+        amount: Int
+    ) {
+        val valueSchema = configuration.getSection("valueTypes")!!
+        val data = mutableListOf<KeyOffsetWithSizeData>()
+        for (x in 0 until amount) {
+            data += KeyOffsetWithSizeDataLoader.readStream(footerStream, configuration, errors)
+        }
+        data.sort()
+        for (x in 0 until amount) {
+            map[data[x].currentKey.toString()] = SchemaDataLoader.represent(dataStream, valueSchema, errors)
+        }
+    }
+
+    private fun parseIntegerKeyWithOffset(
+        map: YamlWrapper.YamlSection,
+        configuration: YamlWrapper.YamlSection,
+        dataStream: DataInputStream,
+        footerStream: DataInputStream,
+        errors: SchemaErrorInfo,
+        amount: Int
+    ) {
+        val valueSchema = configuration.getSection("valueTypes")!!
+
+        val data = mutableListOf<KeyOffsetData>()
+        for (x in 0 until amount) {
+            data += KeyOffsetDataLoader.readStream(footerStream, configuration, errors)
+        }
+        data.sort()
+        for (x in 0 until amount) {
+            map[data[x].currentKey.toString()] = SchemaDataLoader.represent(dataStream, valueSchema, errors)
+        }
+    }
+
+    private fun parseDataKeyMap(
+        map: YamlWrapper.YamlSection,
+        configuration: YamlWrapper.YamlSection,
+        dataStream: DataInputStream,
+        footerStream: DataInputStream,
+        errors: SchemaErrorInfo,
+        amount: Int
+    ) {
+        val footerData = ListLoader.readStream(footerStream, configuration.getSection("keyTypes")!!, errors)
+        val valueSchema = configuration.getSection("valueTypes")!!
+        for (x in 0 until amount) {
+            map[footerData[x]!!] = SchemaDataLoader.represent(dataStream, valueSchema, errors)
+        }
+    }
+
 
 }
